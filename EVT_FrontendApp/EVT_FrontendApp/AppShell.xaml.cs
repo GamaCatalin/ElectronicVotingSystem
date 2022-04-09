36ed0b0dc24f75651e812dde87908fdf2ceedc7c
Catalin Gama <catalin.gama@ngi.systems>
2022-04-09 13:17:29 +0300

    Base Frontend App

---
+++ EVT_FrontendApp/EVT_FrontendApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using EVT_FrontendApp.ViewModels;
+using EVT_FrontendApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EVT_FrontendApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ EVT_FrontendApp/EVT_FrontendApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EVT_FrontendApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace EVT_FrontendApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
