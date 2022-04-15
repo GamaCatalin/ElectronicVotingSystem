13a26f2361b8433459d821bf3bc74643f581ffe1
Catalin Gama <catalin.gama@ngi.systems>
2022-04-15 18:22:49 +0300

    added server component

---
+++ EVT_Server/Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+using EVT_Server.Models;
+using EVT_Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EVT_Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+        
+    }
+
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll()
+    {
+        return PizzaService.GetAll();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        if (pizza == null)
+        {
+            return NotFound();
+        }
+
+        return pizza;
+    }
+}
---
+++ EVT_Server/Models/Pizza.cs
@@ -0,0 +1,8 @@
+namespace EVT_Server.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+}
